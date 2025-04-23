45e8233e5a55a8c47360dada12a29bc5f5f98332
autumn_Middlebrooks_Willis <autumnm3841@gmail.com>
2025-04-23 11:38:33 -0400

    Initial commit with Employee class and PayrollCalculator

---
+++ PayrollCalculator/src/main/java/com/pluralsight/Employee.java
@@ -0,0 +1,41 @@
+package com.pluralsight;
+
+public class Employee {
+
+        private int employeeId;
+        private String name;
+        private double hoursWorked;
+        private double payRate;
+
+    public Employee(int employeeId, String name, double hoursWorked, double payRate) {
+            this.employeeId = employeeId;
+            this.name = name;
+            this.hoursWorked = hoursWorked;
+            this.payRate = payRate;
+        }
+
+        public int getEmployeeId() {
+            return employeeId;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public double getHoursWorked() {
+            return hoursWorked;
+        }
+
+        public double getPayRate() {
+            return payRate;
+        }
+
+        public double getGrossPay() {
+            return hoursWorked * payRate;
+        }
+
+        @Override
+        public String toString() {
+            return "Employee ID: " + employeeId + ", Name: " + name + ", Gross Pay: $" + getGrossPay();
+        }
+    }
---
+++ PayrollCalculator/src/main/java/com/pluralsight/PayrollCalculator.java
@@ -0,0 +1,43 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class PayrollCalculator {
+    public static void main(String[] args) {
+
+        ArrayList<Employee> employees = new ArrayList<>();
+        String filePath = "employee_data.csv";
+
+        try {
+            FileReader fileReader = new FileReader(filePath);
+            BufferedReader bufReader = new BufferedReader(fileReader);
+
+            String line;
+            bufReader.readLine();  // Skip the header line if present
+
+            while ((line = bufReader.readLine()) != null) {
+                String[] data = line.split("\\|");
+
+                int id = Integer.parseInt(data[0]);
+                String name = data[1];
+                double hoursWorked = Double.parseDouble(data[2]);
+                double payRate = Double.parseDouble(data[3]);
+
+                Employee employee = new Employee(id, name, hoursWorked, payRate);
+                employees.add(employee);
+            }
+
+            bufReader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        for (Employee employee : employees) {
+            System.out.printf("Employee ID: %d, Name: %s, Gross Pay: $%.2f\n",
+                    employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
+        }
+    }
+}
